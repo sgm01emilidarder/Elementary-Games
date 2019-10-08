4231b54bb93868eb6d7e9f17d1bcede6f1321c22
sgm01emilidarder <sgm01@iesemilidarder.com>
2019-10-08 22:08:17 +0200

    1

---
+++ js/classes.js
@@ -0,0 +1,11 @@
+class Game {
+  constructor(nom, llançament, pegi, desenvolupador, genere) {
+    this.nom = nom;
+    this.llançament = llançament;
+    this.pegi = pegi;
+    this.desenvolupador = desenvolupador;
+    this.genere = genere;
+  };
+};
+
+gameList =
