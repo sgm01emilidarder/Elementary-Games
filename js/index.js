6fda81a2205be9a72b6fa83a315990f8a263a3ef
sgm01emilidarder <sgm01@iesemilidarder.com>
2019-10-13 20:10:36 +0200

    1

--- js/index.js
+++ js/index.js
@@ -1,33 +1,24 @@
 function validateCamps(nom, llançament, pegi, desenvolupador, genere) {
   if (nom === '' || llançament === '' || pegi === '' || desenvolupador === '' || genere === '') {
-    alert('Es necesario rellenar todos los campos del formulario');
+    message('Es necesario rellenar todos los campos del formulario');
     return false;
   } else {
     return true;
   };
 };
 
-function validateDate(date) {
-  if (/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/.test(date)) {
-    return true;
-  } else {
-    alert('La fecha que has introducido no es válida');
+function validatePegi(pPegi) {
+  if (pPegi > 18) {
+    message("El valor de pegi debe ser un numerico entre 0 y 18")
     return false;
-  };
-};
-
-function validatePegi(pegi) {
-  if (pegi === 3 || pegi === 7 || pegi === 12 || pegi === 16 || pegi === 18) {
-    return true;
   } else {
-    alert('La edad que has introducido no es válida');
-    return false;
-  };
-};
+    return true;
+  }
+}
 
 function checkDuplicateGame(pMap, pNom) {
   if (pMap === pNom) {
-    alert("el juego seleccionado ya esta inscrito en el torneo");
+    message("el juego seleccionado ya esta inscrito en el torneo");
     return false;
   } else {
     return true;
